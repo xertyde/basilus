b359ae1d7423633697a099197c5aa756bee452e6
Nauticare <165992431+xertyde@users.noreply.github.com>
2025-05-26 10:48:22 +0200

    Update portfolio images

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
   },
   images: { 
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
   webpack: (config, { isServer }) => {
     config.cache = false;
