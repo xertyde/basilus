a8993026a022b1970aae758b7d8b2d4be8433a5c
thomas <thomasfonferrier@gmail.com>
2025-10-27 13:25:41 +0100

    r

--- app/realisations/metadata.ts
+++ app/realisations/metadata.ts
@@ -3,4 +3,4 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Nos Réalisations | Basilus',
   description: 'Découvrez les sites web que nous avons créés pour nos clients.',
-} 
+}  
